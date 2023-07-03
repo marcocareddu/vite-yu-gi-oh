6ae5ae5262180f006f7470cf17afdcd5854cb099
marcocareddu <saldumaccu@tuta.io>
2023-07-03 16:02:42 +0200

    VUE  App - Linked Components and moved mokup

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,16 @@
 <script>
+import AppMain from './components/AppMain.vue';
+
 export default {
   name: 'Pokévuex',
+  components: { AppMain },
 
 };
 </script>
 
 <template>
-  <main class="container">
-
-    <!-- Header -->
-    <header class="text-center">
-      <h1>PokéVuex</h1>
-    </header>
-
-    <!-- Section -->
-    <section class="card-box rounded-3 d-flex justify-content-center align-items-center p-5">
-      <div class="card-container rounded-3 h-100 w-100 row">
-
-        <!-- Card / Col -->
-        <!-- !TO INJECT -->
-        <div class="col col-3 g-3 ">
-          <div class="card rounded-5">
-            <img src="https://picsum.photos/200" class="card-img-top img-fluid rounded-5" alt="...">
-            <div class="card-body text-center">
-              <p class="card-text">Number</p>
-              <h5 class="card-text">Name</h5>
-              <p class="card-text">Type1</p>
-            </div>
-          </div>
-        </div>
-
-      </div>
-    </section>
-  </main>
+  <!-- AppMain -->
+  <AppMain />
 </template>
 
 <style lang="scss">
