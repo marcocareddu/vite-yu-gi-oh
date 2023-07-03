c186b07e0d3b3029085b8c3f0741cc2a41f1c1b8
marcocareddu <saldumaccu@tuta.io>
2023-07-03 16:46:37 +0200

    VUE AppCard - Linked to store & V-For

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -1,17 +1,20 @@
 <script>
+import { store } from '../assets/data/store';
 export default {
-
+    data() {
+        return { store }
+    }
 };
 </script>
 
 <template>
-    <div class="col col-3 g-3 ">
+    <div v-for="pokemon in store.pokemon" class="col col-3 g-3 ">
         <div class="card rounded-5">
-            <img src="https://picsum.photos/200" class="card-img-top img-fluid rounded-5" alt="...">
+            <img :src="pokemon.imageUrl" class="card-img-top img-fluid rounded-5" :alt="pokemon.name">
             <div class="card-body text-center">
-                <p class="card-text">Number</p>
-                <h5 class="card-text">Name</h5>
-                <p class="card-text">Type1</p>
+                <p class="card-text">{{ pokemon.number }}</p>
+                <h5 class="card-text">{{ pokemon.name }}</h5>
+                <p class="card-text">{{ pokemon.type1 }}</p>
             </div>
         </div>
     </div>
