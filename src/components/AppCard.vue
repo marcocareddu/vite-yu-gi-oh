308d84209ed503d27a2610d67b051b4c8529bd05
marcocareddu <saldumaccu@tuta.io>
2023-07-03 16:04:00 +0200

    VUE AppCard -  Moved Mokup into

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -4,6 +4,17 @@ export default {
 };
 </script>
 
-<template></template>
+<template>
+    <div class="col col-3 g-3 ">
+        <div class="card rounded-5">
+            <img src="https://picsum.photos/200" class="card-img-top img-fluid rounded-5" alt="...">
+            <div class="card-body text-center">
+                <p class="card-text">Number</p>
+                <h5 class="card-text">Name</h5>
+                <p class="card-text">Type1</p>
+            </div>
+        </div>
+    </div>
+</template>
 
 <style lang="scss" scoped></style>
