c7de820231345f7b970f191e0a96efeee5b67891
marcocareddu <saldumaccu@tuta.io>
2023-07-03 16:46:18 +0200

    VUE App - Imported from API

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,19 @@
 <script>
+import axios from 'axios';
 import AppMain from './components/AppMain.vue';
+const endpoint = 'https://41tyokboji.execute-api.eu-central-1.amazonaws.com/dev/api/v1/pokemons';
+import { store } from './assets/data/store';
 
 export default {
   name: 'Pokévuex',
   components: { AppMain },
+
+  created() {
+    axios.get(endpoint)
+      .then(res => {
+        store.pokemon = res.data.docs;
+      })
+  },
 
 };
 </script>
