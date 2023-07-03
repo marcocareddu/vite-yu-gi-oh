0fc4d95341d99a417a96e49005d1780955ce8ca0
marcocareddu <saldumaccu@tuta.io>
2023-07-03 16:03:29 +0200

    VUE - Added AppMain and moved main into

---
+++ src/components/AppMain.vue
@@ -0,0 +1,46 @@
+<script>
+import AppCard from './AppCard.vue';
+
+export default {
+    components: { AppCard }
+}
+
+</script>
+
+<template>
+    <main class="container">
+
+        <!-- Header -->
+        <header class="text-center">
+            <h1>PokéVuex</h1>
+        </header>
+
+        <!-- Section -->
+        <section class="card-box rounded-3 d-flex justify-content-center align-items-center p-5">
+            <div class="card-container rounded-3 h-100 w-100 row">
+
+                <!-- AppCard / Col -->
+                <!-- !TO INJECT -->
+                <AppCard />
+            </div>
+        </section>
+    </main>
+</template>
+
+<style>
+/* Main */
+/* Header */
+header {
+    height: 80px;
+}
+
+.card-box {
+    background-color: lightblue;
+    height: 700px;
+}
+
+.card-container {
+    background-color: lightgray;
+    overflow-y: auto;
+}
+</style>
