ff2bfe0ec155f788894c4255a078b288e8c5410c
marcocareddu <saldumaccu@tuta.io>
2023-07-03 17:43:25 +0200

    VUE App - Added isLoading flag on API call

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,12 @@ export default {
   created() {
     axios.get(endpoint)
       .then(res => {
+        store.isLoadiung = true;
         store.pokemon = res.data.docs;
+      }).catch(err => {
+        console.err(err.message);
+      }).then(() => {
+        store.isLoadiung = false;
       })
   },
 
