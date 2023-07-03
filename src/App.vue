0e6446f59fb40072d911ef21c1b6c52e06b1c3f3
marcocareddu <saldumaccu@tuta.io>
2023-07-03 15:54:59 +0200

    VUE App - Added Mokup

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,31 @@ export default {
 
 <template>
   <main class="container">
+
+    <!-- Header -->
+    <header class="text-center">
+      <h1>PokéVuex</h1>
+    </header>
+
+    <!-- Section -->
+    <section class="card-box rounded-3 d-flex justify-content-center align-items-center p-5">
+      <div class="card-container rounded-3 h-100 w-100 row">
+
+        <!-- Card / Col -->
+        <!-- !TO INJECT -->
+        <div class="col col-3 g-3 ">
+          <div class="card rounded-5">
+            <img src="https://picsum.photos/200" class="card-img-top img-fluid rounded-5" alt="...">
+            <div class="card-body text-center">
+              <p class="card-text">Number</p>
+              <h5 class="card-text">Name</h5>
+              <p class="card-text">Type1</p>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </section>
   </main>
 </template>
 
