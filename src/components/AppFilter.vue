075a83e0ef886058e392975beb4d17bd6509d6a5
marcocareddu <saldumaccu@tuta.io>
2023-07-04 14:50:33 +0200

    VUE AppFilter - Create base filter

---
+++ src/components/AppFilter.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+    props: { typeToSearch: Array },
+    data() {
+        return {
+            stringToSearch: '',
+            elementToSearch: '',
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="searh-bar d-flex justify-content-center m-3">
+        <input type="text" v-model.trim="stringToSearch" @keyup.enter>
+        <select name="" id="" v-model="elementToSearch">
+            <option value="--">--</option>
+            <option v-for="element in typeToSearch" :key="element" :value="element">{{ element }}</option>
+        </select>
+    </div>
+</template>
+
+<style scoped></style>
